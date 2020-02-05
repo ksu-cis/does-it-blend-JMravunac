c8bcbb4478aad4547132be7146a983258277de68
Jack Mravunac <jmravunac@ksu.edu>
2020-02-05 16:28:36 -0600

    class

--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace DoesItBlend
 {
     class Program
@@ -7,6 +7,23 @@ namespace DoesItBlend
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var b = new Banana();
+            var m = new Mango();
+            var s = new Strawberry();
+            var c = new IceCubes();
+
+            Console.Write(b.Blend());
+            Console.Write(m.Blend());
+            Console.Write(s.Blend());
+
+            List<IBlendable> fruits = new List<IBlendable>() { b, m, s, c};
+
+            foreach(var fruit in fruits)
+            {
+                Console.WriteLine(fruit.Blend());
+            }
+
         }
     }
 }
